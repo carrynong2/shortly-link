e8afa8b0e46b3ac19f0ee06b1cb8e8582f516ca8
carrynong2 <carrynong2@gmail.com>
2023-10-15 17:16:25 +0700

    first commit

---
+++ backend/go.mod
@@ -0,0 +1,8 @@
+module shorty-link
+
+go 1.16
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	gorm.io/gorm v1.25.5
+)
---
+++ backend/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Shortlylink struct {
+	gorm.Model
+	OriginalURL string `gorm: "unique"`
+	ShortURL string `gorm: "unique"`
+}
+
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + 
+		":" + dbPort + ")/" + dbName + "?parseTime=true"
+	
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&Shortlylink{})
+
+	r := gin.Default()
+
+	r.Use(cors.Default())
+
+}
